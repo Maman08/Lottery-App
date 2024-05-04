8654be28c36e676529dc0cd1439fb42894a1a742
mritunjay8 <151187227+mritunjay8@users.noreply.github.com>
2024-05-04 12:47:23 +0530

    first commit

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,4 +1,37 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
 
-contract Lottery {}
+contract Lottery{
+    address payable[] public players;
+    address manager;
+    address payable public winner;
+    constructor(){
+        manager=msg.sender;
+    } 
+    receive() external payable { 
+        require(msg.value==1 ether,"Please pay 1 ether only");
+        players.push(payable(msg.sender)); 
+    }
+    function grtBalance() public view  returns (uint){
+        require(manager==msg.sender,"Your are not the manager");
+        return address(this).balance;
+    }
+    function random() internal view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.basefee,block.timestamp,players.length)));
+    }
+    function pickWinner()public {
+        require(msg.sender==manager,"You are not the manager");
+        require(players.length>=3,"Player aare less than 3");
+        uint r= random();
+        uint index= r%players.length;
+        winner=players[index];
+        winner.transfer(grtBalance());
+        players= new address payable[](0); 
+    }
+    function allPlayers() public view returns(address payable[] memory ){
+        return players;
+    } 
+}
+
+
+// 0xF5a7a8f8C874789Cc43D1fc1914752Fa99Ec9dad
--- truffle-config.js
+++ truffle-config.js
@@ -19,7 +19,7 @@
  */
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
+// const mnemonic="wet that step vanish heavy reject illness monkey raven then mountain fury"
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -28,17 +28,17 @@ module.exports = {
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
    * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * run develop or test. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
    * $ truffle test --network <network-name>
    */
   contracts_build_directory: "./client/src/contracts",
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
+    // Useful for testing. The development name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // tab if you use this network and you must also set the host, port and network_id
     // options below to some value.
     //
     development: {
@@ -57,17 +57,18 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // Sepolia: {
+    //   provider: () => new HDWalletProvider(mnemonic, "https://eth-sepolia.g.alchemy.com/v2/f-E7WPEQMZ3mJHuDctEa2AkhMWdFWugF"),
+    //   network_id: 11155111,       // Sepolia's id
+    //   gas: 5500000,        // Adjust gas limit as necessary
+    //   confirmations: 2,    // Number of confirmations to wait between deployments
+    //   timeoutBlocks: 200,  // Number of blocks before a deployment times out
+    //   skipDryRun: true     // Skip dry run before migrations
     // },
+    
     // Useful for private networks
     // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // provider: () => new HDWalletProvider(mnemonic, https://network.io),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
